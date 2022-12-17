04cdb99e7989581b2f8a2eac7e9e72a177a2a6ca
Omar Hassan <omar.hassan@tuni.fi>
2022-12-17 15:33:00 +0200

    Initial tool code

--- src/cliport_label/console.py
+++ src/cliport_label/console.py
@@ -1,13 +1,11 @@
 """CLI entrypoints for cliport_label"""
-import logging
+from importlib import import_module
 
 import click
 
-from libadvian.logging import init_logging
 from cliport_label import __version__
+from cliport_label.main import main
 
-
-LOGGER = logging.getLogger(__name__)
 
 
 @click.command()
@@ -16,11 +14,5 @@ LOGGER = logging.getLogger(__name__)
 @click.option("-v", "--verbose", count=True, help="Shorthand for info/debug loglevel (-v/-vv)")
 def cliport_label_cli(loglevel: int, verbose: int) -> None:
     """GUI application for recording demonstration for CLIPort project """
-    if verbose == 1:
-        loglevel = 20
-    if verbose >= 2:
-        loglevel = 10
-    init_logging(loglevel)
-    LOGGER.setLevel(loglevel)
+    main()
 
-    click.echo("Do your thing")
